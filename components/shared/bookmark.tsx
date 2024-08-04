24bd7496e82e5cbdeb7de3f489bc70617088fa92
Tiltusha <elcoronabooking@gmail.com>
2024-08-04 15:01:56 +0300

    ?

---
+++ components/shared/bookmark.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+import { cn } from '@/lib/utils'
+
+const Bookmark = () => {
+    return (
+        <div>
+            <h1>Bookmark</h1>
+        </div>
+    )
+}
+
